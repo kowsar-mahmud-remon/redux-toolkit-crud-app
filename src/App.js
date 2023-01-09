fde03786f6f1e50771ef37bcfaf30a59c12c72b8
creativerze <creativerze@gmail.com>
2023-01-09 23:18:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Index from './routes';
 
 function App() {
   return (
     <div className="App">
-
+      <h2>app</h2>
+      <Index></Index>
     </div>
   );
 }
